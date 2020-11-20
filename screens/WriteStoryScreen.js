e15eb480c40dd250402e1dc28e74355b57d5f5b5
saikeerthana-123 <63954676+saikeerthana-123@users.noreply.github.com>
2020-11-20 08:54:20 +0530

    Add files via upload

---
+++ screens/WriteStoryScreen.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, TextInput,StyleSheet, TouchableOpacity } from 'react-native';
+
+export default class WriteScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <p>Story Title</p>
+          <TextInput style={styles.inputBox} />
+          <p>Story Author</p>
+          <TextInput style={styles.inputBox} />
+          <p>Write Your Story</p>
+          <TextInput style={styles.inputBox} />
+          <TouchableOpacity 
+              style={styles.scanButton}>
+              <Text style={styles.buttonText}>Submit</Text>
+            </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+  const styles = StyleSheet.create({
+    inputBox: {
+      marginTop: 100,
+      width: '80%',
+      alignSelf: 'center',
+      height: 40,
+      textAlign: 'center',
+      borderWidth: 2,
+      outline: 'none',
+    } ,
+    scanButton:{
+      backgroundColor:'#EDC0BF',
+      width: 50,
+      borderWidth:1.5,
+     }
+  })
+  
